5e35b4f68fe33e9dd7a5cb3723128ea916c2a2e9
Kovács Péter <kovacs.peter@students.jedlik.eu>
2024-04-18 13:22:17 +0200

    fix

--- foods_data.js
+++ foods_data.js
@@ -1,7 +1,7 @@
 const foods_data = [
     //"Név;Kategória;Tartalomleírás;Ár;Kép",
     "1;Hízlalt csirke;Szárnyas;Effilé csirke 13 napig érlelve zöldséggel töltve;5750;https://m.blog.hu/af/afickofakanallal/image/toltott_csirk_hizl_erlelt.jpg",
-    "2;Vörös Marhahátszín;Marha;Marhahátszín 5 napig vörösborban érlelve, Krémes tormamártással és steakburgonyával;7650;https://scontent-vie1-1.xx.fbcdn.net/v/t39.30808-6/431498601_924400586356142_319702258585630845_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=FPi4hra3WlAAb67Q6TK&_nc_ht=scontent-vie1-1.xx&oh=00_AfCA6yfqyUXoPxAmi3wcJh5duVnLcEWqeJ1TF8txYJprXg&oe=661AE4E7",
+    "2;Vörös Marhahátszín;Marha;Marhahátszín 5 napig vörösborban érlelve, Krémes tormamártással és steakburgonyával;7650;https://www.mindmegette.hu/images/381/O/_mor2583.jpeg",
     "3;Pulled Pork;Sertés;Sertés tarja fokhagymával érlelve és bükk fával füstölve választhazó körettel;6980;https://www.mindmegette.hu/images/199/O/pulled_pork.jpg ",
     "4;Boros Sertés;Sertés;Fehérboros pácban érlelt sonka zöldségkörettel;4620;https://image-api.nosalty.hu/nosalty/images/recipes/sI/NR/boros-pacban-erlelt-serteshus.jpeg?w=670&fit=crop&fm=webp&crop=424%2C290%2C%2C204&h=460&mark=watermark.png&markpos=bottom-left&markpad=20&markw=&s=e64ab70fe45ff92e8fe9c4a5a9fe960e",
     "5;Amerikai álom;Vegetáriánus;1 hétig pincében tárolt, akáccal füstölt cheddar sajtos mac and cheese falatkák;7100;https://www.mindmegette.hu/images/335/L/lead_L_mac_and_cheese.jpg",
